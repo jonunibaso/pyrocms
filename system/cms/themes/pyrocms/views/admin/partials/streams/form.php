411f6b52c0f8c94d4aa6f92014a638f4ed9aac91
jonunibaso <jon.unibaso@gmail.com>
2012-11-28 16:17:30 +0100

    Update system/cms/themes/pyrocms/views/admin/partials/streams/form.php
    Quick patch for deleting image streams

--- system/cms/themes/pyrocms/views/admin/partials/streams/form.php
+++ system/cms/themes/pyrocms/views/admin/partials/streams/form.php
@@ -13,8 +13,16 @@
 				<br /><small><?php echo $field['instructions']; ?></small>
 			<?php endif; ?>
 			</label>
-			
-			<div class="input"><?php echo $field['input']; ?></div>
+			<div id="input_<?php echo $field['input_slug'];?>">
+			<div class="input"><?php echo $field['input']; 
+			     	if( (strpos($field['input'], "file")>0) && (strpos($field['input'], "img")>0) )
+			{
+				echo "<button id='del_button_".$field['input_slug']."' type='button' onclick=\"delImg('".$field['input_slug']."'); return false;\" >Delete</button>";
+			}
+				
+			?>
+				</div>
+			</div>
 		</li>
 
 	<?php } ?>
@@ -29,5 +37,15 @@
 		<button type="submit" name="btnAction" value="save" class="btn blue"><span><?php echo lang('buttons.save'); ?></span></button>	
 		<a href="<?php echo site_url(isset($return) ? $return : 'admin/streams/entries/index/'.$stream->id); ?>" class="btn gray"><?php echo lang('buttons.cancel'); ?></a>
 	</div>
+
+
+<script>
+ function delImg(slug){
+	$("input[name="+slug+"]").val("-1");
+	$("#input_"+slug).find('.input').find('img').remove();
+	$("#del_button_"+slug).remove();
+}
+</script>
+
 
 <?php echo form_close();?>
